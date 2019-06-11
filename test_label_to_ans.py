2652a6a28251ddea5b19b62682a2ddb19d7681bf
chang-kai-qi <45838568+kevin94705@users.noreply.github.com>
2019-06-12 02:03:50 +0800

    Add files via upload

--- test_label_to_ans.py
+++ test_label_to_ans.py
@@ -7,13 +7,16 @@ with open('test_labels.csv', newline='') as csvfile:
         
     rows = csv.DictReader(csvfile)
     for row in rows:
-        name=row["filename"]        
-        if name in ans_dic.keys():
-            ans_dic[name]=ans_dic[name]+1
-        else:            
-            ans_dic[name]=1
+        name=row["filename"]
+        classes=row["class"]
+        if name in ans_dic.keys():                       
+            if classes in ans_dic[name]:
+                temp=ans_dic[name][classes]+1
+                ans_dic[name][classes]=temp
+            else:
+                ans_dic[name][classes]=1                                                        
+        else:                                
+            ans_dic[name]={classes:1}       
+        
 print(ans_dic)
-'''
- with open('test_label_ans.csv', 'w', newline='') as csvfile:
-        fieldnames = ['filename', "偵測數量" ]
-'''
+
